b1896a512a9a1d8c96da7d62b018faa4a423147c
darkdefender10 <heman.spark@gmail.com>
2017-02-15 23:12:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Minesweeper from './minesweeper';
+import './index.css';
+
+ReactDOM.render(
+  <Minesweeper />,
+  document.getElementById('root')
+);
